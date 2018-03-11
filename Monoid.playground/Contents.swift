662abac9c5859860a736e9de2d5b06fb19f98c63
Roberto Gómez Muñoz <rob.gomez.mu@gmail.com>
2018-03-11 13:47:50 +0100

    code.clean()

--- Monoid.playground/Contents.swift
+++ Monoid.playground/Contents.swift
@@ -1,34 +1,27 @@
 //: Playground - Monoid
 
-class AddUserUseCase {
-    let db = UserDatabase()
-    
-    func add(name: String, password: String) -> Result<User, UserError> {
-        let user = User(name: name, password: password)
-        
-        let validator = validateName && validatePassword
-        
-        return validator(user).map(db.create)
-    }
-
-    internal func validateName(of user: User) -> Result<User, UserError> {
-        if !user.name.isEmpty && user.name.count <= 15 {
-            return .success(user)
-        }
-        
-        return .failure(.userNameOutOfBounds)
+func validatePassword(of user: User) -> Result<User, UserError> {
+    if user.password.count > 10 {
+        return .success(user)
     }
     
-    internal func validatePassword(of user: User) -> Result<User, UserError> {
-        if user.password.count > 10 {
-            return .success(user)
-        }
-        
-        return .failure(.passwordTooShort)
-    }
+    return .failure(.passwordTooShort)
 }
 
-let useCase = AddUserUseCase()
+func validateName(of user: User) -> Result<User, UserError> {
+    if !user.name.isEmpty && user.name.count <= 15 {
+        return .success(user)
+    }
+    
+    return .failure(.userNameOutOfBounds)
+}
 
-let user = useCase.add(name: "alex", password: "functionalswift")
+func createUser(name: String, password: String) -> Result<User, UserError> {
+    let validator = validateName && validatePassword
+    
+    return validator(User(name: name, password: password))
+}
+
+let user = createUser(name: "alex", password: "functionalswift")
+
 user.map { print("SUCCESS: User created - \($0)") }
--- Monoid.playground/Sources/UserDatabase.swift
+++
@@ -1,8 +0,0 @@
-public class UserDatabase {
-    
-    public init() {}
-    
-    public func create(_ user: User) -> User {
-        return user
-    }
-}
