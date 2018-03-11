242c36801ba0d48542dacc152d523c645a0e3582
Roberto Gómez Muñoz <rob.gomez.mu@gmail.com>
2018-03-11 12:04:52 +0100

    optional<User>.map { Let the party go on!!! } but if user is not created... What's gone wrong? :(

--- Monoid.playground/Contents.swift
+++ Monoid.playground/Contents.swift
@@ -3,15 +3,17 @@
 class AddUserUseCase {
     let db = UserDatabase()
     
-    func add(name: String, password: String) -> Bool {
+    func add(name: String, password: String) -> User? {
         if validate(name: name) && validate(password: password) {
-            db.create(User(name: name, password: password))
-            return true
+            let user = User(name: name, password: password)
+            db.create(user)
+            
+            return user
         }
         
-        return false
+        return nil
     }
-    
+
     internal func validate(name: String) -> Bool {
         return !name.isEmpty && name.count <= 15
     }
@@ -24,9 +26,7 @@ class AddUserUseCase {
 
 let useCase = AddUserUseCase()
 
-if useCase.add(name: "alex", password: "functionalswift") {
-    print("SUCCESS: User created")
-} else {
-    print("ERROR: Something is wrong")
-}
+let user = useCase.add(name: "alex", password: "functionalswift")
+user.map { print("SUCCESS: User created - \($0)") }
+
 
